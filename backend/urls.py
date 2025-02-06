e02850d61ed3890f9f2f3b7b65dc6ea279086f1d
Akashkannan05 <akash2005k26kaniyur12@gmail.com>
2025-02-06 11:56:45 +0530

    URL for login

--- backend/urls.py
+++ backend/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('',views.Home.as_view()),
-    path('profile/',views.ProfileDetailViewClass,name="Profile page")
+    path('profile/',views.ProfileDetailViewClass,name="Profile page"),
+    path('login/',views.LoginViewClass,name="Login")
 ]
