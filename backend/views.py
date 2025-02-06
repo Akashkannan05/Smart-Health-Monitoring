823ca427b71883fac3669c6975369f78c69addd9
Akashkannan05 <akash2005k26kaniyur12@gmail.com>
2025-02-06 11:53:03 +0530

    Login

--- backend/views.py
+++ backend/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth import login ,logout,authenticate
+from django.http import JsonResponse
 
-# Create your views here.
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics,views,serializers,status
-from django.http import JsonResponse
+from rest_framework.authtoken.models import Token
+
 from .models import UserProfileModel
 from .serializers import ProfileSerializer
 
@@ -23,9 +24,33 @@ class ProfileDetailView(generics.RetrieveAPIView):
     def get(self, request, *args, **kwargs):
         user=self.request.user
         if user is None or user.is_anonymous:
-            return JsonResponse({"Login":"Not_logined"},status=403)
+            return JsonResponse({"Login":"Not_logined"},status=status.HTTP_403_FORBIDDEN)
         data=UserProfileModel.objects.get(User=user)
         serilaize=ProfileSerializer(data)
         return Response(serilaize.data,status=200)
     
 ProfileDetailViewClass=ProfileDetailView.as_view()
+
+class LoginView(views.APIView):
+
+    permission_classes=[]
+
+    def post(self, request, *args, **kwargs):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        if not username or not password:
+            return Response({"error": "Username and password are required."},status=status.HTTP_400_BAD_REQUEST)
+
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                token = Token.objects.get_or_create(user=user)
+                login(request,user)
+                return Response({"token": token.key}, status=status.HTTP_200_OK)
+            else:
+                return Response({"error": "User account is inactive."},status=status.HTTP_403_FORBIDDEN,)
+        else:
+            return Response({"error": "Invalid username or password."},status=status.HTTP_401_UNAUTHORIZED,)
+
+LoginViewClass=LoginView.as_view()
